be10fc4e24ad180f0a5c655c4c6f3dd742997d58
NguyenBaPhucITITIU20278 <91875515+NguyenBaPhucITITIU20278@users.noreply.github.com>
2025-05-13 19:28:47 +0700

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 // API URLs
-export const API_URL = 'http://localhost:8080';
+export const API_URL = 'https://apartment-backend-30kj.onrender.com';
 
 // MoMo Payment Configuration
 export const MOMO_CONFIG = {
